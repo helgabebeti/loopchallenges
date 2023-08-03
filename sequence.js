d874a3dcea033f27d9c938470e59ebd0db4de153
helgabebeti <93710228+helgabebeti@users.noreply.github.com>
2023-08-03 09:02:06 +0200

    Add files via upload

---
+++ sequence.js
@@ -0,0 +1,6 @@
+var value = 16;
+
+for (var i = 0; i < value; i++) {
+    if(i == value % 4 )
+    console.log(i);
+}
---
+++ sigma.js
@@ -0,0 +1,7 @@
+var sum=0;
+const x = 100;
+
+for(var i=1;i<=100;i++){
+    sum += i;
+}
+console.log("sum:" + sum);
